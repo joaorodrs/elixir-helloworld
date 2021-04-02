1dcf97158bb8ae1dda25ce171484b37f4fa97e04
João Paulo <jpjoao1001@gmail.com>
2021-04-02 17:39:34 -0300

    added helloworld functions

--- lib/elixir_helloworld.ex
+++ lib/elixir_helloworld.ex
@@ -9,4 +9,8 @@ defmodule ElixirHelloworld do
 
   def par?(number) when rem(number, 2) == 0, do: IO.puts('É um número par') # better than IF/ELSE
   def par?(_number), do: IO.puts('Não é um número par')
+
+  def hello(name), do: "Oi #{name}"
+  def hello(name1, name2), do: "Oi #{name1} e #{name2}"
+  def hello, do: "Nome não foi dado"
 end

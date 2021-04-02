d1b3ea313e1ca8380666045ba0d2f9048a40842e
João Paulo <jpjoao1001@gmail.com>
2021-04-02 17:21:38 -0300

    added even/pair checker

--- lib/elixir_helloworld.ex
+++ lib/elixir_helloworld.ex
@@ -1,9 +1,12 @@
 defmodule ElixirHelloworld do
-  def par?(number) do
-    if rem(number, 2) == 0 do
-      true
-    else
-      false
-    end
-  end
+  # def par?(number) do
+  #   if rem(number, 2) == 0 do
+  #     true
+  #   else
+  #     false
+  #   end
+  # end
+
+  def par?(number) when rem(number, 2) == 0, do: IO.puts('É um número par') # better than IF/ELSE
+  def par?(_number), do: IO.puts('Não é um número par')
 end
